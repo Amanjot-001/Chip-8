40b20f8f2d4c0fcf73a326172794a889c4de5b6a
Amanjot-001 <amanjotsingh2309@gmail.com>
2024-11-08 23:10:22 +0530

    fix: memory read and write

--- pkg/cpu/cpu.go
+++ pkg/cpu/cpu.go
@@ -3,7 +3,6 @@ package cpu
 import (
 	"chip-8/pkg/memory"
 	"log"
-	"os"
 )
 
 type CPU struct {
@@ -14,37 +13,43 @@ type CPU struct {
 	Stack     []uint16       // stack
 }
 
-func(cpu *CPU) Reset(gamePath string) {
+func NewCPU(gamePath string) *CPU {
+	cpu := &CPU{
+		Memory: memory.NewMemory(),
+		PC:     0x200, // Programs start at 0x200
+		Stack:  make([]uint16, 16),
+	}
+	cpu.Reset()
+	cpu.LoadGame(gamePath)
+	return cpu
+}
+
+func (cpu *CPU) Reset() {
 	cpu.I = 0                      // addr register to 0
 	cpu.PC = 0x200                 // starting addr of all games
 	for i := range cpu.Registers { // initialize all registers to 0
 		cpu.Registers[i] = 0
 	}
-	for i := range cpu.Memory { // mem cleared
-		cpu.Memory[i] = 0
-	}
-	cpu.Stack = make([]uint16, 16) // reinit stack with 0
 
-	gameData, err := os.ReadFile(gamePath) // game read
+	cpu.Stack = make([]uint16, 16) // reinit stack with 0
+	cpu.Memory.Clear()             // mem clear
+}
+
+func (cpu *CPU) LoadGame(gamePath string) {
+	err := cpu.Memory.LoadGame(gamePath, 0x200)
 	if err != nil {
 		log.Fatalf("Failed to load game: %v", err)
 	}
-
-	if len(gameData) > len(cpu.Memory)-0x200 { // not fit in memory
-		log.Fatalf("Game too large to fit in memory.")
-	}
-
-	copy(cpu.Memory[0x200:], gameData)
 }
 
 // 2 bytes opcode but 1 byte mem size
 // combining PC and PC+1 to get opcode (big-endian)
 func (cpu *CPU) GetNextOpcode() uint16 {
 	var res uint16 = 0
-	res = uint16(cpu.Memory[cpu.PC])    // first byte
-	res <<= 8                           // left shift 8
-	res |= uint16(cpu.Memory[cpu.PC+1]) // second byte
-	cpu.PC += 2                         // increment PC
+	res = uint16(cpu.Memory.Read(cpu.PC))      // first byte
+	res <<= 8                                  // left shift 8
+	res |= uint16(cpu.Memory.Read(cpu.PC + 1)) // second byte
+	cpu.PC += 2                                // increment PC
 
 	return res
 }
--- pkg/memory/memory.go
+++ pkg/memory/memory.go
@@ -1,5 +1,7 @@
 package memory
 
+import "os"
+
 type Memory struct {
 	data [4096]uint8
 }
@@ -12,4 +14,31 @@ func (m *Memory) Clear() {
 	for i := range m.data {
 		m.data[i] = 0
 	}
+}
+
+func (m *Memory) Read(address uint16) uint8 {
+	if address < 4096 {
+		return m.data[address]
+	}
+	return 0
+}
+
+func (m *Memory) Write(address uint16, value uint8) {
+	if address < 4096 {
+		m.data[address] = value
+	}
+}
+
+func (m *Memory) LoadGame(gamePath string, startAddress uint16) error {
+	gameData, err := os.ReadFile(gamePath)
+	if err != nil {
+		return err
+	}
+
+	if len(gameData) > len(m.data)-int(startAddress) {
+		return err
+	}
+
+	copy(m.data[startAddress:], gameData)
+	return nil
 }
