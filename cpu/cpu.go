0cdd8ffe5ade685bd72ddc6b5681948049efca15
Amanjot-001 <amanjotsingh2309@gmail.com>
2024-11-08 22:38:31 +0530

    instr decode and execution

--- cpu/cpu.go
+++ cpu/cpu.go
@@ -47,3 +47,25 @@ func (cpu *CPU) GetNextOpcode() uint16 {
 
 	return res
 }
+
+func (cpu *CPU) DecodeAndExecute(opcode uint16) {
+	first := opcode & 0xF000
+	second := opcode & 0x0F00
+	third := opcode & 0x00F0
+	fourth := opcode & 0x000F
+	last3 := opcode & 0x0FFF
+	last2 := opcode & 0x00FF
+
+	switch first {
+	case 0x0000:
+
+	case 0x1000:
+		cpu.PC = last3
+	case 0x6000:
+		register := (opcode & 0x0F00) >> 8
+		value := uint8(last2)
+		cpu.Registers[register] = value
+	default:
+		log.Printf("Unknown opcode: 0x%X\n", opcode)
+	}
+}
