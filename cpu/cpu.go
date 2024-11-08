e7bc0b6825ae8f513ff54c573b7762d8859bbea0
Amanjot-001 <amanjotsingh2309@gmail.com>
2024-11-08 15:58:27 +0530

    cpu reset

--- cpu/cpu.go
+++ cpu/cpu.go
@@ -1,9 +1,21 @@
 package cpu
 
 type CPU struct {
-	Memory    [4096]uint8
-	Registers [16]uint8
-	I         uint16
-	PC        uint16
-	Stack     []uint16
+	Memory    [4096]uint8 // 4096 bytes of memory
+	Registers [16]uint8   // 16 8-bit registers
+	I         uint16      // addr register
+	PC        uint16      // program counter
+	Stack     []uint16    // stack
+}
+
+func Reset(cpu *CPU) {
+	cpu.I = 0                      // addr register to 0
+	cpu.PC = 0x200                 // starting addr of all games
+	for i := range cpu.Registers { // initialize all registers to 0
+		cpu.Registers[i] = 0
+	}
+	for i := range cpu.Memory { // mem cleared
+		cpu.Memory[i] = 0
+	}
+	cpu.Stack = make([]uint16, 16) // reinit stack with 0
 }
