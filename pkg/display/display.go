44621ed721b6e6f06628d86c31e1b1b341448918
Amanjot-001 <amanjotsingh2309@gmail.com>
2024-11-10 20:26:55 +0530

    Add: Draw sprites method

--- pkg/display/display.go
+++ pkg/display/display.go
@@ -20,3 +20,26 @@ func (d *Display) Clear() {
 		}
 	}
 }
+
+func (d *Display) DrawSprite(x, y uint8, sprite []uint8) bool {
+	collision := false
+	for row := 0; row < len(sprite); row++ {
+		spriteRow := sprite[row]
+		for col := 0; col < 8; col++ {
+			pixelX := (x + uint8(col)) % Width
+			pixelY := (y + uint8(row)) % Height
+
+			pixelState := (spriteRow >> (7 - col)) & 1
+
+			// both are 1 then resulting pixel will be 0 (means VF = 1)
+			if d.Pixels[pixelY][pixelX] == 1 && pixelState == 1 {
+				collision = true
+			}
+
+			// pixel toggle
+			d.Pixels[pixelY][pixelX] ^= pixelState
+		}
+	}
+
+	return collision
+}
