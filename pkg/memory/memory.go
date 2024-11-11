077bca44532fefd461cf191079dbd019ee103173
Amanjot-001 <amanjotsingh2309@gmail.com>
2024-11-11 19:43:32 +0530

    Add: Fontset in memory

--- pkg/memory/memory.go
+++ pkg/memory/memory.go
@@ -1,18 +1,29 @@
 package memory
 
-import "os"
+import (
+	"chip-8/pkg/utils"
+	"os"
+)
 
 type Memory struct {
 	data [4096]uint8
 }
 
 func NewMemory() *Memory {
-	return &Memory{}
+	m := &Memory{}
+	m.LoadFontset()
+	return m
 }
 
 func (m *Memory) Clear() {
 	for i := range m.data {
 		m.data[i] = 0
+	}
+}
+
+func (m *Memory) LoadFontset() {
+	for i := 0; i < len(utils.Fontset); i++ {
+		m.data[i+0x0050] = utils.Fontset[i]
 	}
 }
 
