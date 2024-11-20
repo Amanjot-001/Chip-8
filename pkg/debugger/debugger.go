b21ef0a9c820bc20e57eacc2a94ea1e6612c94c1
Amanjot-001 <amanjotsingh2309@gmail.com>
2024-11-20 19:23:25 +0530

    add: keys print in debugger

--- pkg/debugger/debugger.go
+++ pkg/debugger/debugger.go
@@ -3,6 +3,9 @@ package debugger
 import (
 	"chip-8/pkg/cpu"
 	"fmt"
+	"log"
+
+	"github.com/veandco/go-sdl2/sdl"
 )
 
 type Debugger struct {
@@ -24,6 +27,7 @@ func (d *Debugger) PrintState() {
 		}
 	}
 	fmt.Printf("\nDelayTimer: %d  SoundTimer: %d\n", d.CPU.DelayTimer, d.CPU.SoundTimer)
+	d.PrintKeyStates()
 	fmt.Println("\nStack:")
 	for i := 0; i < int(d.CPU.SP); i++ {
 		fmt.Printf("0x%04X ", d.CPU.Stack[i])
@@ -35,5 +39,33 @@ func (d *Debugger) PrintMemory(start, end uint16) {
 	fmt.Printf("---- Memory Dump (0x%04X to 0x%04X) ----\n", start, end)
 	for addr := start; addr <= end; addr++ {
 		fmt.Printf("0x%04X: 0x%02X\n", addr, d.CPU.Memory.Read(addr))
+	}
+}
+
+func (d *Debugger) PrintKeyStates() {
+	fmt.Println("\nKey States:")
+	for i, key := range d.CPU.Keys.Keys {
+		if key == 1 {
+			fmt.Printf("Key %X: PRESSED\n", i)
+		} else {
+			fmt.Printf("Key %X: RELEASED\n", i)
+		}
+	}
+}
+
+func (d *Debugger) WaitForKeyPress(quit *bool) {
+	for {
+		event := sdl.WaitEvent()
+		switch e := event.(type) {
+		case *sdl.QuitEvent:
+			*quit = true
+			return
+		case *sdl.KeyboardEvent:
+			if e.State == sdl.PRESSED {
+				log.Printf("Key pressed: %v\n", e.Keysym.Sym)
+				// d.CPU.Keys.HandleKeyPress(e) // Update the key state
+				return
+			}
+		}
 	}
 }
